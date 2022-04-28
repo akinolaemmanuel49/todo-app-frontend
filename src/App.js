5f74c65df45e87673390dcb5c9214377cbf0a832
Abiodun Akinola <akinolaemmanuel49@gmail.com>
2022-04-28 09:48:00 +0100

    returning to zero

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React from "react";
 import AppBar from "./components/AppBar";
-import Button from "./components/Button";
+import TodoList from "./todo/TodoList";
 import "./App.css";
 
 const App = () => {
-  return (
-    <>
-      <div className="app">
-        <div className="background">
-          <AppBar name="John Doe" />
-          <div className="container"><p><div className="content">
-
-          </div>
-          </p>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  return <></>;
 };
 export default App;
