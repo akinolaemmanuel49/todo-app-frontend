8059296e25e9243d529b95d25afd612583e95ccb
Abiodun Akinola <akinolaemmanuel49@gmail.com>
2022-04-29 21:17:43 +0100

    Implement todo item component

---
+++ src/components/TodoItem.js
@@ -0,0 +1,17 @@
+import "./TodoItem.css";
+import DoneButton from "./DoneButton";
+import deleteSignImage from "../images/deleteSign.svg";
+
+const TodoItem = () => {
+  return (
+    <div className="todoItemInner">
+      <DoneButton />
+      <input className="todoItemInput" type="text" />
+      <button className="todoDeleteItemButton">
+        <img src={deleteSignImage} alt="ADD" />
+      </button>
+    </div>
+  );
+};
+
+export default TodoItem;
