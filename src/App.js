4e92ad6e2646ce50e5832e2e0f8f3674c4098c82
Abiodun Akinola <akinolaemmanuel49@gmail.com>
2022-04-29 21:19:41 +0100

    implement mockup display

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import AddTodoItem from "./components/AddTodoItem";
+import TodoItem from "./components/TodoItem";
+import TodoList from "./todo/TodoList";
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      <AddTodoItem />
+      <NavBar
+        username="JOHNHALSEY"
+        profileImage="https://images.unsplash.com/photo-1618828665347-d870c38c95c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFnb3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+      />
+      <TodoList />
     </>
   );
 };
