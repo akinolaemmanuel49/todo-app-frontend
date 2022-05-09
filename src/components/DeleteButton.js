f6b9faab1a8b39e380ae497791944d40afa7fa20
Abiodun Akinola <akinolaemmanuel49@gmail.com>
2022-05-09 22:39:15 +0100

    fixed delete button event

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -4,20 +4,24 @@ import API_URL from "../utils/constants";
 import deleteSignImage from "../images/deleteSign.svg";
 import "./DeleteButton.css";
 import authHeader from "../services/auth-header";
+import UserService from "../services/user.service";
 
 const DeleteButton = (props) => {
-  const deleteRequest = () => {
-    axios
-      .delete(API_URL + "/todos/" + props.todoId, {
-        headers: {
-          Authorization: authHeader(),
-        },
-      })
-      .then(() => {});
-  };
+  // const deleteRequest = () => {
+  //   axios
+  //     .delete(API_URL + "/todos/" + props.todoId, {
+  //       headers: {
+  //         Authorization: authHeader(),
+  //       },
+  //     })
+  //     .then(() => {});
+  // };
+
   const handleClick = (e) => {
-    deleteRequest();
+    // deleteRequest();
+    UserService.deleteTodo(props.todoId);
   };
+
   return (
     <button className="todoDeleteItemButton" onClick={handleClick}>
       <img src={deleteSignImage} alt="DELETE BUTTON" />
