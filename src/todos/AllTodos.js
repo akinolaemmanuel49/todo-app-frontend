c6ea760f99e5ef5ec9b5caee157470a46e2f9f50
Abiodun Akinola <akinolaemmanuel49@gmail.com>
2022-05-14 10:45:29 +0100

    authentication bugfix

--- src/todos/AllTodos.js
+++ src/todos/AllTodos.js
@@ -5,29 +5,37 @@ import AddTodoItem from "../components/AddTodoItem";
 import TodoList from "../components/TodoList";
 import AuthService from "../services/auth.service";
 import NavBar from "../components/NavBar";
+import { isTokenExpired } from "../utils/isTokenExpired";
 
 const AllTodos = () => {
   const [loadedTodoList, setLoadedTodoList] = useState([]);
   useEffect(() => {
-    AuthService.currentUserProfile().then((res) => {
-      if (res.data.username) {
-        TodoService.fetchTodoList()
-          .then((res) => {
-            const todoList = [];
-            for (const key in res.data) {
-              const todoItem = {
-                id: key,
-                ...res.data[key],
-              };
-              todoList.push(todoItem);
-            }
-            setLoadedTodoList(todoList);
-          })
-          .catch((err) => {
-            console.error(err);
-          });
-      }
-    });
+    if (!isTokenExpired(localStorage.getItem("accessToken"))) {
+      AuthService.currentUserProfile()
+        .then((res) => {
+          if (res.data.username) {
+            TodoService.fetchTodoList().then((res) => {
+              const todoList = [];
+              for (const key in res.data) {
+                const todoItem = {
+                  id: key,
+                  ...res.data[key],
+                };
+                todoList.push(todoItem);
+              }
+              setLoadedTodoList(todoList);
+            });
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+          window.location.replace("/signin");
+        });
+    } else if (!isTokenExpired(localStorage.getItem("refreshToken"))) {
+      AuthService.refreshTokenAPI();
+    } else {
+      window.location.replace("/signin");
+    }
   }, []);
   return (
     <>
